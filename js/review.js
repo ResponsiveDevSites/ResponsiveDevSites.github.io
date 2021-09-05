e4a8985cfb5e92d4fd1cc9de796ca0ca3c811c8a
ResponsiveDevSites <responsivedevsites@gmail.com>
2021-09-05 21:26:46 +0530

    Added common js

--- js/review.js
+++ js/review.js
@@ -1,40 +1,17 @@
-var sheetAPIBaseURL = "https://sheets.googleapis.com/v4/spreadsheets"
-var ExcelfileID = "1ydx9B9s00Jp_Q0PNjQOLERtUC-eAYm6S5_VKBDvBdwA";
-var apiKey = "AIzaSyALYSx_H8_qXnLRJvuql7GzfTZ3MvlLdqI";
 
-/*Read categories*/
-var categorySheetName = "Categories";
-var categoryFromRange = "A1";
-var categoryToRange = "A12";
-
-var readCategoryURL = sheetAPIBaseURL + "/" + ExcelfileID + "/values/" + categorySheetName + "!" + categoryFromRange + ":" + categoryToRange + "?key=" + apiKey;
-/*Read categories ends*/
-
-/*Read products*/
-var productSheetName = "Products";
-var productFromRange = "A1";
-var productToRange = "E12";
-var readProductURL = sheetAPIBaseURL + "/" + ExcelfileID + "/values/" + productSheetName + "!" + productFromRange + ":" + productToRange + "?key=" + apiKey;
-/*Read products ends*/
-
-/*Read products*/
-var productVariantsSheetName = "ProductVariants";
-var productVariantsFromRange = "A1";
-var productVariantsToRange = "F20";
-var readProductVariantsURL = sheetAPIBaseURL + "/" + ExcelfileID + "/values/" + productVariantsSheetName + "!" + productVariantsFromRange + ":" + productVariantsToRange + "?key=" + apiKey;
-/*Read products ends*/
-
-var categoryResult = null;
-var productResult = null;
-var productVariantsResult = null;
 
 $(document).ready(function () {
 
     categoryResult = JSON.parse(localStorage.getItem("categoryResult"));
     productResult = JSON.parse(localStorage.getItem("productResult"));
+    productVariantsResult = JSON.parse(localStorage.getItem("productVariantsResult"));
 
     if (categoryResult == null || categoryResult == '') {
         getCategoriesAjax();
+        categoryResult = JSON.parse(localStorage.getItem("categoryResult"));
+        loadMenuCategories(categoryResult);
+        loadSearchCategories(categoryResult);
+        loadMobileViewMenuCat(categoryResult);
     }
     else {
         loadMenuCategories(categoryResult);
@@ -43,93 +20,21 @@ $(document).ready(function () {
     }
     if (productResult == null || productResult == '') {
         getProductsAjax();
+        productResult = JSON.parse(localStorage.getItem("productResult"));
+    }
+    else {
+        productResult = JSON.parse(localStorage.getItem("productResult"));
     }
     if (productVariantsResult == null || productVariantsResult == '') {
         getProductVariantsAjax();
+        productVariantsResult = JSON.parse(localStorage.getItem("productVariantsResult"));
     }
     else {
         loadReviewCart();
     }
     updateCartCount();
 });
-
-function getCategoriesAjax() {
-
-    $.ajax({
-        type: "GET",
-        url: readCategoryURL,
-        cache: false,
-        dataType: "json",
-        success: function (data) {
-            categoryResult = data.values.slice(1); //removed first row. it contains column title
-            localStorage.setItem("categoryResult", JSON.stringify(categoryResult));
-            loadMenuCategories(categoryResult);
-            loadSearchCategories(categoryResult);
-
-            loadMobileViewMenuCat(categoryResult);
-        }
-    });
-}
-function getProductsAjax() {
-
-    $.ajax({
-        type: "GET",
-        url: readProductURL,
-        cache: false,
-        dataType: "json",
-        success: function (data) {
-            productResult = data.values.slice(1); //removed first row. it contains column title
-            localStorage.setItem("productResult", JSON.stringify(productResult));
-
-
-        }
-    });
-
-}
-function getProductVariantsAjax() {
-    $.ajax({
-        type: "GET",
-        url: readProductVariantsURL,
-        cache: false,
-        dataType: "json",
-        success: function (data) {
-            productVariantsResult = data.values.slice(1); //removed first row. it contains column title
-            localStorage.setItem("productVariantsResult", JSON.stringify(productVariantsResult));
-            loadReviewCart();
-        }
-    });
-
-}
-
-function loadMenuCategories(categories) {
-    var catPart1 = '<li><a href="javascript:" onclick="navigateToProducts(\'All\')" >All Categories</a></li>';
-    var catPart2 = '';
-    var partition = Math.round((categories.length / 2));
-
-    for (var i = 0; i < partition; i++) {
-        catPart1 += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[i] + '\')"> ' + categories[i] + '</a ></li >';
-    }
-    for (var j = partition; j < categories.length; j++) {
-        catPart2 += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[j] + '\')"> ' + categories[j] + '</a ></li >';
-    }
-    $('#catPart1').html(catPart1);
-    $('#catPart2').html(catPart2);
-}
-function loadSearchCategories(categories) {
-    var ddlOptions = "<option value=''>All Categories</option>";
-    for (var i = 0; i < categories.length; i++) {
-        ddlOptions += "<option value=''>" + categories[i] + "</option>";
-    }
-    $('#searchCat').html(ddlOptions);
-}
-function loadMobileViewMenuCat(categories) {
-    var mobileViewMenuCat = '<li><a href="javascript:" onclick="navigateToProducts(\'All\')" >All Categories</a></li>';
-    for (var i = 0; i < categories.length; i++) {
-        var encodedURL = encodeURIComponent(categories[i]);
-        mobileViewMenuCat += '<li><a href="javascript:" onclick="navigateToProducts(\'' + categories[i] + '\')">' + categories[i] + '</a></li>';
-        $('#mobileViewMenuCat').html(mobileViewMenuCat);
-    }
-}
+ 
 function loadReviewCart() {
     var cartObj = [];
 
@@ -194,26 +99,6 @@ function clearCart() {
         }
     }, 1000);
 }
-function getUserAgent() {
-    var txt = navigator.userAgent;
-    txt += "<br>Resolution: " + Math.round(window.screen.width) + "x" + Math.round(window.screen.height);
-    txt += "<br>Browser Online: " + navigator.onLine;
-    $('#userAgent').html(txt);
-}
-function updateCartCount() {
-    if (localStorage.getItem("cart") != null && localStorage.getItem("cart") != "") {
-        var cartObj = JSON.parse(localStorage.getItem("cart"));
-        $('#cartItemCount').html(cartObj.length);
-    }
-    else {
-        $('#cartItemCount').html('0');
-    }
-}
-
-function navigateToProducts(Category) {
-    sessionStorage.setItem("selectedCategory", Category);
-    window.location.href = "products.html";
-}
 function submitOrder() {
 
     $("#msg-container").removeClass('hide');
@@ -235,7 +120,6 @@ $(document).on('click', '.btn-xs-delete', function () {
     $('#hdnValueToDelete').val($(this).attr('data-rowindex'))
     $('#confirmDelete').modal('show');
 });
-
 function deleteCartRow() {
     var cartRowIndex = $('#hdnValueToDelete').val();
     var cartObj = JSON.parse(localStorage.getItem("cart"));
