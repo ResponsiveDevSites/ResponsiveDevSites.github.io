558435bc56a2218d6520b15f18636955c2f02d25
ResponsiveDevSites <responsivedevsites@gmail.com>
2021-10-24 12:07:02 +0530

    Height issue fixed. one js reference is added

--- js/productgallery.js
+++ js/productgallery.js
@@ -1,6 +1,6 @@
 var variantCollection = [];
 var slideIndex = 1;
-var productImageBasePath = "https://responsivedevsites.github.io";
+var productImageBasePath = window.location.protocol + "//" + window.location.hostname;
 
 $(document).ready(function () {
 
