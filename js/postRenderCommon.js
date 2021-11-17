b8b0df613bc78e483718f27bdaa06a0193ac7b09
ResponsiveDevSites <responsivedevsites@gmail.com>
2021-11-17 22:40:52 +0530

    keypress  does not work on mobile

--- js/postRenderCommon.js
+++ js/postRenderCommon.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     //regex to allow only alphanumeric characters
     var regEx = "^[a-zA-Z0-9]+$";
 
-    $('.validate-chars').keypress(function (e) {
+    $('.validate-chars').on('keyup', function (e) {
         var regex = new RegExp(regEx);
         var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
         if (regex.test(str)) {
